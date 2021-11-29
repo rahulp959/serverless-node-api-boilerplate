efd8f56a6380d78f35c4d591e608347ec761d2e8
Rahul Parkar <rahul.a.parkar@gmail.com>
2021-11-29 01:50:52 +0000

    Add Prettier, Husky and lint-staged

--- src/api/hello/handler.ts
+++ src/api/hello/handler.ts
@@ -1,12 +1,15 @@
-import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/apiGateway';
-import { formatJSONResponse } from '@libs/apiGateway';
-import { middyfy } from '@libs/lambda';
+import type { ValidatedEventAPIGatewayProxyEvent } from "@libs/apiGateway";
+import { formatJSONResponse } from "@libs/apiGateway";
+import { middyfy } from "@libs/lambda";
 
-import schema from './schema';
+import schema from "./schema";
 
-const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => formatJSONResponse({
-  message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
-  event,
-});
+const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (
+  event
+) =>
+  formatJSONResponse({
+    message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
+    event,
+  });
 
 export default middyfy(hello);
--- src/libs/lambda.ts
+++ src/libs/lambda.ts
@@ -1,4 +1,5 @@
-import middy from '@middy/core';
-import middyJsonBodyParser from '@middy/http-json-body-parser';
+import middy from "@middy/core";
+import middyJsonBodyParser from "@middy/http-json-body-parser";
 
-export const middyfy = (handler: any) => middy(handler).use(middyJsonBodyParser());
+export const middyfy = (handler: any) =>
+  middy(handler).use(middyJsonBodyParser());
