89227b11c15008ca69100eba97970287e7d2ebb1
Rahul Parkar <rahul.a.parkar@gmail.com>
2021-11-29 01:42:21 +0000

    Add ESLint and some common rules

--- src/api/hello/handler.ts
+++ src/api/hello/handler.ts
@@ -1,14 +1,12 @@
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/apiGateway';
 import { formatJSONResponse } from '@libs/apiGateway';
-import { middyfy } from '@libs/lambda'
+import { middyfy } from '@libs/lambda';
 
 import schema from './schema';
 
-const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
-  return formatJSONResponse({
-    message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
-    event,
-  });
-}
+const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => formatJSONResponse({
+  message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
+  event,
+});
 
-export const main = middyfy(hello);
+export default middyfy(hello);
